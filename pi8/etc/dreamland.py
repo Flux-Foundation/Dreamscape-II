700ca2e6b636ed34f37189362dd931c2ab5a4ec4
Samson Yeung <fragmede@onepatchdown.net>
2016-06-10 06:46:24 +0000

    wip - include /etc/dreamland.py config.

---
+++ pi8/etc/dreamland.py
@@ -0,0 +1,1 @@
+config={'master': '192.168.1.122', 'input': {'giraffe_side': {'pin': 10}}, 'topic': 'dreamscape2', 'output': {'button_light': {'pin': 8}, 'flame': {'pin': 11}}, 'hostname': 'pi8.local'}
